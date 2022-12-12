f1444ad4dd81ca8862637292f366d84a56e0c08e
karpukhinda <karpuhinda@yandex.ru>
2022-12-12 16:39:32 +0300

    сохр очередное

--- Program.cs
+++ Program.cs
@@ -65,7 +65,6 @@
 // NumberRowMinSumElements(table);
 
 
-// // Функция вывода номера строки (не индекса) с наименьшей суммой элементов 
 // void NumberRowMinSumElements(int[,] array)
 // {
 //     int minRow = 0;
@@ -88,7 +87,6 @@
 //     Console.Write($"строка с наименьшей суммой элементов : {minSumRow + 1}");
 // }
 
-// // Функция вывода двумерного массива
 // void PrintArray(int[,] array)
 // {
 //     for (int i = 0; i < array.GetLength(0); i++)
@@ -101,7 +99,6 @@
 //     }
 // }
 
-// // Функция заполнения массива рандомно числами от 1 до 9
 // void FillArrayRandom(int[,] array)
 // {
 //     for (int i = 0; i < array.GetLength(0); i++)
@@ -114,24 +111,160 @@
 // }
 
 
-//Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// //Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 
-int InputInt(string output)
+// int InputInt(string output)
+// {
+//     Console.Write(output);
+//     return int.Parse(Console.ReadLine());
+// }
+
+// void FillArrayRandomNumbers(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(1, 5); //Для увеличения размера чисел в матрицах поменять число 5 на большее
+//         }
+//     }
+// }
+
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         Console.Write("[ ");
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.Write("]");
+//         Console.WriteLine("");
+//     }
+// }
+
+// int size = InputInt("размерность матриц: ");
+// int[,] matrixA = new int[size, size];
+// int[,] matrixB = new int[size, size];
+// FillArrayRandomNumbers(matrixA);
+// FillArrayRandomNumbers(matrixB);
+// int[,] matrixC = new int[size, size];
+
+// for (int i = 0; i < size; i++)
+// {
+//     for (int j = 0; j < size; j++)
+//     {
+//         for (int k = 0; k < size; k++)
+//         {
+//             matrixC[i, j] = matrixC[i, j] + (matrixA[i, k] * matrixB[k, j]);
+//         }
+//     }
+// }
+// Console.WriteLine("Матрица - А");
+// PrintArray(matrixA);
+// Console.WriteLine();
+// Console.WriteLine("Матрица - В");
+// PrintArray(matrixB);
+// Console.WriteLine();
+// Console.WriteLine("Произведение матриц А*В");
+// PrintArray(matrixC);
+
+// //Задача 60 Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить 
+// //массив, добавляя индексы каждого элемента.
+// int deep1 = InputInt("Введите размерность 1: ");
+// int deep2 = InputInt("Введите размерность 2: ");
+// int deep3 = InputInt("Введите размерность 3: ");
+// int countNums = 89;
+
+// if (deep1 * deep2 * deep3 > countNums)
+// {
+//     Console.Write("Массив слишком большой");
+//     return;
+// }
+
+// int[,,] resultNums = Create3DMassive(deep1, deep2, deep3);
+
+// for (int i = 0; i < resultNums.GetLength(0); i++)
+// {
+//     for (int j = 0; j < resultNums.GetLength(1); j++)
+//     {
+//         for (int k = 0; k < resultNums.GetLength(2); k++)
+//         {
+//             Console.WriteLine($"[{i},{j},{k}] - {resultNums[i, j, k]}");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+
+// int[,,] Create3DMassive(int size1, int size2, int size3)
+// {
+//     int[,,] array = new int[size1, size2, size3];
+//     int[] values = new int[countNums];
+//     int num
+//      = 10;
+//     for (int i = 0; i < values.Length; i++)
+//         values[i] = num
+//         ++;
+
+//     for (int i = 0; i < values.Length; i++)
+//     {
+//         int randomInd = new Random().Next(0, values.Length);
+//         int temp = values[i];
+//         values[i] = values[randomInd];
+//         values[randomInd] = temp;
+//     }
+
+//     int valueIndex = 0;
+
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             for (int k = 0; k < array.GetLength(2); k++)
+//             {
+//                 array[i, j, k] = values[valueIndex++];
+//             }
+//         }
+//     }
+//     return array;
+// }
+
+
+// int InputInt(string output)
+// {
+//     Console.Write(output);
+//     return int.Parse(Console.ReadLine());
+// }
+
+
+// //Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[,] nums = new int[size, size];
+
+int num = 1;
+int i = 0;
+int j = 0;
+
+while (num <= size * size)
 {
-    Console.Write(output);
-    return int.Parse(Console.ReadLine());
+    nums[i, j] = num;
+    if (i <= j + 1 && i + j < size - 1)
+        ++j;
+    else if (i < j && i + j >= size - 1)
+        ++i;
+    else if (i >= j && i + j > size - 1)
+        --j;
+    else
+        --i;
+    ++num;
 }
 
-void FillArrayRandomNumbers(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = new Random().Next(1, 5); //Для увеличения размера чисел в матрицах поменять число 5 на большее
-        }
-    }
-}
+PrintArray(nums);
 
 void PrintArray(int[,] array)
 {
@@ -147,28 +280,4 @@ void PrintArray(int[,] array)
     }
 }
 
-int size = InputInt("размерность матриц: ");
-int[,] matrixA = new int[size, size];
-int[,] matrixB = new int[size, size];
-FillArrayRandomNumbers(matrixA);
-FillArrayRandomNumbers(matrixB);
-int[,] matrixC = new int[size, size];
 
-for (int i = 0; i < size; i++)
-{
-    for (int j = 0; j < size; j++)
-    {
-        for (int k = 0; k < size; k++)
-        {
-            matrixC[i, j] = matrixC[i, j] + (matrixA[i, k] * matrixB[k, j]);
-        }
-    }
-}
-Console.WriteLine("Матрица - А");
-PrintArray(matrixA);
-Console.WriteLine();
-Console.WriteLine("Матрица - В");
-PrintArray(matrixB);
-Console.WriteLine();
-Console.WriteLine("Произведение матриц А*В");
-PrintArray(matrixC);
